a0a5a83994bcf6a821ca0ecb3df24747dddbf857
Matthew Gleich <git@mattglei.ch>
2021-06-06 01:02:03 -0400

    feat: add "meet the family" section
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,25 +9,30 @@ export default function Home() {
         <title>blackbird</title>
       </Head>
       <main className={styles.main}>
-        <div className={`${styles.planes}`}>
-          <div className="animate__delay-1s animate__animated animate__fadeInDown">
-            <BlackbirdPlane />
+        <div className={styles.welcome}>
+          <div className={`${styles.planes}`}>
+            <div className="animate__delay-1s animate__animated animate__fadeInDown">
+              <BlackbirdPlane />
+            </div>
+            <div
+              className={`${styles.middlePlane} animate__delay-2s animate__animated animate__fadeInDown`}
+            >
+              <BlackbirdPlane />
+            </div>
+            <div className="animate__delay-3s animate__animated animate__fadeInDown">
+              <BlackbirdPlane />
+            </div>
           </div>
-          <div
-            className={`${styles.middlePlane} animate__delay-2s animate__animated animate__fadeInDown`}
-          >
-            <BlackbirdPlane />
-          </div>
-          <div className="animate__delay-3s animate__animated animate__fadeInDown">
-            <BlackbirdPlane />
-          </div>
-        </div>
-        <div>
           <h1
             className={`${styles.hello} animate__delay-5s animate__animated animate__fadeInUp`}
           >
             Say hello to <span className={styles.blackbird}>blackbird</span>
           </h1>
+          <div className={styles.spacer} />
+        </div>
+        <div>
+          <h1 style={{ marginBottom: "10px" }}>Meet the family</h1>
+          <img className={styles.colors} src="colors.svg" alt="colors" />
         </div>
       </main>
     </div>
