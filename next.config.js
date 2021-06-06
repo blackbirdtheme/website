2448ae3a0cff9f5f2f8b13de1d5faf0bc5d816bd
Matthew Gleich <git@mattglei.ch>
2021-06-05 23:08:34 -0400

    feat: add animated planes
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+const path = require("path");
 
 /** @type {import('next/dist/next-server/server/config').NextConfig} */
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias['~'] = path.resolve('./src')
-    return config
+    config.resolve.alias["~"] = path.resolve("./src");
+    return config;
   },
-}
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import BlackbirdPlane from "../components/BlackbirdPlane";
 
 export default function Home() {
   return (
@@ -8,7 +9,19 @@ export default function Home() {
         <title>blackbird</title>
       </Head>
       <main className={styles.main}>
-        <h1>Hello</h1>
+        <div className={`${styles.planes}`}>
+          <div className="animate__delay-1s animate__animated animate__fadeInDown">
+            <BlackbirdPlane />
+          </div>
+          <div
+            className={`${styles.middlePlane} animate__delay-2s animate__animated animate__fadeInDown`}
+          >
+            <BlackbirdPlane />
+          </div>
+          <div className="animate__delay-3s animate__animated animate__fadeInDown">
+            <BlackbirdPlane />
+          </div>
+        </div>
       </main>
     </div>
   );
